43eb910d72367976515435157613be5815a498d3
Kamppix <42889487+Kamppix@users.noreply.github.com>
2022-05-30 20:35:38 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ copy "dist\main.exe" "bin\PixelMusicPacker.exe" /y
 copy "app_icon.ico" "bin\app_icon.ico" /y
 copy "ffmpeg.exe" "bin\ffmpeg.exe" /y
 copy "run.bat" "bin\run.bat" /y
-xcopy "music_packs\" "bin\music_packs\" /y /e /i
+xcopy "template_pack\" "bin\template_pack\" /y /e /i
 rmdir dist /s /q
 rmdir build /s /q
 rmdir __pycache__ /s /q
